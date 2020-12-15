0c900f12322fb3543225ce258abeab7cf352bb24
Enamul Hassan <enamsustcse@gmail.com>
2020-12-15 16:36:38 +0600

    Template Solution
    Students should write their solution in this file.

---
+++ solution.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int fibo(int n)
+{
+  int ret;
+  // your code goes here
+  
+  return ret;
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  cout << fibo(n) << endl;
+  return 0;
+}
